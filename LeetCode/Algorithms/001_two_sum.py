d8c3a360e0d8560215291ea6e5e4c7f21102b97d
Renyu Chen <jeffreyfox.chen@gmail.com>
2026-02-25 21:39:54 -0800

    two sum

--- LeetCode/Algorithms/001_two_sum.py
+++ LeetCode/Algorithms/001_two_sum.py
@@ -8,3 +8,13 @@ class Solution:
                 return [idx, i]
             d[val] = i
         return [-1, -1]
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        value_to_index: Dict[int, int] = {}
+        for i, value in enumerate(nums):
+            idx = value_to_index.get(target-value, -1)
+            if idx >= 0:
+                return [idx, i]
+            value_to_index[value] = i
+        return [-1, -1]
