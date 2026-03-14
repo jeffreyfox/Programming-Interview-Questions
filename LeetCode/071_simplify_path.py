2221ea3fe745ce668289d987bd1d8f94fdef63b5
Renyu Chen <jeffreyfox.chen@gmail.com>
2026-03-14 13:03:44 -0700

    071 simplify path

---
+++ LeetCode/071_simplify_path.py
@@ -0,0 +1,14 @@
+# Solution using a stack
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        for part in path.split("/"):
+            if part == "" or part == ".":
+                continue
+            if part == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(part)
+        return "/" + "/".join(stack)
