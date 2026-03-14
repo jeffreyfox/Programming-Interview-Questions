5b298fba4003a1782babf45fec4ea611f1419f2f
Renyu Chen <jeffreyfox.chen@gmail.com>
2026-03-14 08:39:27 -0700

    path sum

---
+++ LeetCode/Algorithms/112_path_sum.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Note the corner cases. Empty tree should return False.
+# Also the recursion should stop at a leaf node, not a None node
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None:
+            return False
+
+        if root.left is None and root.right is None:
+            return targetSum == root.val
+
+        targetSum -= root.val
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
