e69aedd217870447e8f8edaaee47e36bef7dc85c
Renyu Chen <jeffreyfox.chen@gmail.com>
2026-03-23 20:51:27 -0700

    binary

---
+++ LeetCode/278_first_bad_version.py
@@ -0,0 +1,17 @@
+# TAGS:binary search
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+# classic binary search
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        lo, hi = 1, n
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if isBadVersion(mid):
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return lo
+        
