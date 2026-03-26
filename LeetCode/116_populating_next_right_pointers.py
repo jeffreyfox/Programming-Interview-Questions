33e1253f644fc7138b20ae6be97c478708b747e8
Renyu Chen <jeffreyfox.chen@gmail.com>
2026-03-25 21:25:03 -0700

    next pointers

---
+++ LeetCode/116_populating_next_right_pointers.py
@@ -0,0 +1,59 @@
+# TAGS: tree,BFS
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+# level order traversal. Works for general case (117)
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if root is None:
+            return root
+        
+        q = deque([root])
+        while q:
+            prev = None
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                if prev:
+                    prev.next = node
+                prev = node
+        return root
+
+# O(1) solution using the fact that the tree is perfect.
+# Take advantage of the next pointers at the previous level
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if root is None:
+            return root
+        
+        leftmost = root
+        while leftmost.left:
+            curr = leftmost
+            while curr:
+                curr.left.next = curr.right
+                if curr.next:
+                    curr.right.next = curr.next.left
+                curr = curr.next
+            leftmost = leftmost.left
+        return root
